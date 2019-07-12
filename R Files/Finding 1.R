566b69123fa85a129afd94804a2a72ccc2e16ac2
taddio <tadsc.holmes@gmail.com>
2019-07-12 16:21:10 -0700

    Update Finding 1.R

--- R Files/Finding 1.R
+++ R Files/Finding 1.R
@@ -1,7 +1,7 @@
 library(data.table)
 #Importing large dataset
-dataset = fread("participant_checkin_2.csv")
-dataset = dataset[, -1]
+participant_checkin = fread("participant_checkin_2.csv")
+participant_checkin = participant_checkin[, -1]
 
 #Importing smaller dataset
 library(readr)
@@ -9,6 +9,7 @@ Club_site = read_csv("Club_site.csv")
 
 library(readr)
 Customer_agreement = read_csv("Customer_Agreement.csv")
+Customer_agreement = Customer_agreement[, -1]
 
 #---------------------------------------
 
@@ -32,13 +33,13 @@ for(i in 1:nrow(Club_site)){
 }
 
 Club_site$club_type_id = as.integer(Club_site$club_type_id) #converiting columns from character to integer 
-dataset$club_type_id = as.integer(Club_site$club_type_id)  
+participant_checkin$club_type_id = as.integer(Club_site$club_type_id)  
 
 #-----------------------------------------
 
 #Using new ID numbers for dataset
-club_nums = dataset$club_num
-club_types = dataset$club_num
+club_nums = participant_checkin$club_num
+club_types = participant_checkin$club_num
 club_src_nums = Club_site[, c(1, 10)]
 
 for(i in 1:nrow(club_src_nums)){
@@ -51,14 +52,14 @@ for(i in 1:nrow(club_src_nums)){
 
 #Simplifying Participant ID
 
-dataset$simple_participant_id = 0
-simple_participant_id = dataset$simple_participant_id
-participant_id_list = dataset$participant_id
+participant_checkin$simple_participant_id = 0
+simple_participant_id = participant_checkin$simple_participant_id
+participant_id_list = participant_checkin$participant_id
 
 
-participant_id = dataset$participant_id[1] #starting from first ID
+participant_id = participant_checkin$participant_id[1] #starting from first ID
 integer = 1
-for(i in 1:(nrow(dataset))){
+for(i in 1:(nrow(participant_checkin))){
   if(participant_id_list[i] == participant_id){
     simple_participant_id[i] = integer #if ID same as previous ID, enter same simplified ID
   }
@@ -69,7 +70,7 @@ for(i in 1:(nrow(dataset))){
   }
 }
 
-dataset$simple_participant_id = simple_participant_id
+participant_checkin$simple_participant_id = simple_participant_id
 
 #-------------------------------------------
 
@@ -78,13 +79,13 @@ dataset$simple_participant_id = simple_participant_id
 #install.packages("tidyverse")
 library(tidyverse)
 
-setkey(dataset, simple_participant_id)
+setkey(participant_checkin, simple_participant_id)
 
 
 participant = 1
 participant_membership = c()
 for(i in 1:72339){ #72339 different particpants
-  participant = dataset[simple_participant_id == i] #Subset for each particpant
+  participant = participant_checkin[simple_participant_id == i] #Subset for each particpant
   participant_club_types = participant$club_types #Find column of gym types they attended
   ultra_sport_check = which(participant_club_types %in% 4) #Find indices of ultra-sport attendances
   super_sport_check = which(participant_club_types %in% 3) #Find indices of super-sport attendances
@@ -100,7 +101,7 @@ for(i in 1:72339){ #72339 different particpants
   participant_membership = c(participant_membership, participant$membership)
 }
 
-dataset$membership_type = participant_membership
+participant_checkin$membership_type = participant_membership
 
 #----------------------------------------------------
 #Finding Duration of Membership
@@ -137,8 +138,7 @@ Customer_agreement$cleaned_termination_date = new_end_dates
 #----------------------------------------------
 # Finding first and final checkin time
 
-checkin_times = dataset$checkin_datetime_localtime
-colMax = function(data) sapply(data, max, na.rm = TRUE)
+checkin_times = participant_checkin$checkin_datetime_localtime
 max_checkin = max(checkin_times)
 min_checkin = min(checkin_times)
 
@@ -153,23 +153,63 @@ start_dates = Customer_agreement$cleaned_agreement_sign_dt
 end_dates = Customer_agreement$cleaned_termination_date
 
 date_diff = c()
-final_date = "2019-02-17"
+final_date = "2019-02-17" #Final attendance entry in dataset
 for(i in 1:(length(start_dates))){
   if(is.na(end_dates[i])){
-    date_diff = c(date_diff, as.Date(as.character(final_date), format="%Y/%m/%d")-
-                    as.Date(as.character(start_dates[i]), format="%Y/%m/%d"))
+    date_diff = c(date_diff, difftime(final_date, start_dates[i]))
   }
   else{
-    date_diff = c(date_diff, as.Date(as.character(end_dates[i]), format="%Y/%m/%d")-
-    as.Date(as.character(start_dates[i]), format="%Y/%m/%d"))
+    date_diff = c(date_diff, difftime(end_dates[i], start_dates[i]))
   }
 }
 
-print(date_diff)
+Customer_agreement$duration_of_membership = date_diff
+
+Customer_agreement$duration_of_membership_month = (Customer_agreement$duration_of_membership)/30.4375
+
+date_diff = Customer_agreement$duration_of_membership
+date_diff_2 = date_diff
+start_date_cutoff = "2006-12-31"
+for(i in 1:(length(start_dates))){
+  if(start_dates[i] < start_date_cutoff){
+    if(is.na(end_dates[i])){
+      date_diff_2[i] = difftime(final_date, start_date_cutoff)
+    }
+    else{
+      date_diff_2[i] = difftime(end_dates[i], start_date_cutoff)
+    }
+  }
+}
+
+Customer_agreement$duration_of_membership_cutoff = date_diff_2
+Customer_agreement$duration_of_membership_month_cutoff = (Customer_agreement$duration_of_membership_cutoff)/30.4375
 
 #--------------------------------------------------
+#Finding Average Attendance for dataset
+library(plyr)
+
+participant_ids = Customer_agreement$participant_id
+participant_checkin_ids = participant_checkin$participant_id
+
+table = table(participant_checkin_ids)
+attendance_frequencies = as.data.frame(table)
+frequencies = attendance_frequencies$Freq
+frequencies_ids = attendance_frequencies$participant_checkin_ids
+
+total_attendance = participant_checkin_ids
+for(i in 1:length(participant_ids)){
+  index = which(frequencies_ids %in% participant_ids[i])
+  if(length(index)!= 0){
+    total_attendance = c(total_attendance, frequencies[index])
+  }
+  else{
+    total_attendance = c(total_attendance, 0)
+  }
+}
+
+
 #Save Files
-write.csv(dataset, "participant_checkin_2.csv")
+write.csv(participant_checkin, "participant_checkin_2.csv")
 
 write.csv(Customer_agreement, "Customer_agreement.csv")
 
